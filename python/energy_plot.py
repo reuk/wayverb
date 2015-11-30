e0f38fcaeb4cdcb9b9ba4b847f80eb59c39d0427
reuk <reuben.thomas@me.com>
2015-11-30 21:49:46 +0000

    error calculations

--- python/energy_plot.py
+++ python/energy_plot.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from string import split
+import math
 
 def main():
     bands = 7
@@ -15,12 +16,21 @@ def main():
                 max_vals.append(max_val)
 
         m = max(energies)
-        #energies = [e / m for e in energies]
+        energies = [e / m for e in energies]
 
         theta = np.linspace(0, 2 * np.pi, len(energies), False)
 
+        shape = 0.5
+        desired = [(1 - shape) + shape * np.cos(i) for i in theta]
+
+        errors = [np.abs((b - a) / b) for a, b in zip(energies, desired)]
+        errors = [val for val in errors if not math.isnan(val)]
+        print "max error:", max(errors)
+        print "mean error:", sum(errors) / len(errors)
+
         ax = plt.subplot(241 + i, projection='polar')
         ax.plot(theta, energies, color='r', linewidth = 3)
+        ax.plot(theta, desired, color='g', linewidth = 3)
         #ax.plot(theta, max_vals, color='b', linewidth = 3)
         #ax.set_rmax(max(max(energies), max(max_vals)))
         ax.grid(True)
