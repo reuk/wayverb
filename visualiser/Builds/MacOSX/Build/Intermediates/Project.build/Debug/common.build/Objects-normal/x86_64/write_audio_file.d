99ea3057ddf18adc341f6be987cd17ee8a15cf49
reuk <reuben.thomas@me.com>
2015-12-18 17:38:17 +0000

    oops, removed build

--- visualiser/Builds/MacOSX/Build/Intermediates/Project.build/Debug/common.build/Objects-normal/x86_64/write_audio_file.d
+++
@@ -1,3 +0,0 @@
-dependencies: /Users/reuben/dev/waveguide/common/write_audio_file.cpp \
-  /Users/reuben/dev/waveguide/common/write_audio_file.h \
-  /usr/local/include/sndfile.hh /usr/local/include/sndfile.h
--- visualiser/Builds/MacOSX/Build/Intermediates/Project.build/Debug/waveguide.build/Objects-normal/x86_64/tetrahedral_program.d
+++
@@ -1,3 +0,0 @@
-dependencies: /Users/reuben/dev/waveguide/lib/tetrahedral_program.cpp \
-  /Users/reuben/dev/waveguide/lib/tetrahedral_program.h \
-  /Users/reuben/dev/waveguide/include/cl.hpp
