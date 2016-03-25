f0bf48f68df2c88d91580500f8bb3ca227f4a77e
reuk <reuben.thomas@me.com>
2016-03-25 14:46:59 +0000

    tried to make faster with memoization, didn't work lol

--- waveguide/rectangular_mesh.h
+++ waveguide/rectangular_mesh.h
@@ -12,6 +12,17 @@
 
 #include <vector>
 #include <set>
+#include <unordered_map>
+
+namespace std {
+template <typename T, typename U>
+class hash<std::pair<T, U>> {
+public:
+    size_t operator()(const std::pair<T, U>& p) const {
+        return hash<T>()(p.first) ^ hash<U>()(p.second);
+    }
+};
+};
 
 class RectangularMesh : public BaseMesh<RectangularProgram, Vec3i> {
 public:
@@ -30,24 +41,27 @@ public:
     template <int I, typename B>
     std::vector<RectangularProgram::BoundaryDataArray<I>> compute_boundary_data(
         const B& b) const {
+        ConnectedFinder cf;
         std::vector<RectangularProgram::BoundaryDataArray<I>> ret(
             compute_num_boundary<I>());
 
-        for (const auto& i : get_nodes()) {
-            if (i.boundary_type != RectangularProgram::id_reentrant &&
-                popcount(i.boundary_type) == I) {
+        for (auto i = 0u; i != get_nodes().size(); ++i) {
+            const auto& node = get_nodes()[i];
+            if (node.boundary_type != RectangularProgram::id_reentrant &&
+                popcount(node.boundary_type) == I) {
                 auto count = 0;
                 for (auto j = 0; j != PORTS; ++j) {
                     auto bits =
                         RectangularProgram::port_index_to_boundary_type(j);
-                    if (i.boundary_type & bits) {
-                        auto connected =
-                            look_for_connected(b, i, get_nodes(), bits);
+                    if (node.boundary_type & bits) {
+                        auto connected = cf.look_for_connected_memoized(
+                            b, i, get_nodes(), bits);
                         assert(connected.type == bits ||
                                connected.type ==
                                    RectangularProgram::id_reentrant);
-                        ret[i.boundary_index].array[count++].coefficient_index =
-                            connected.index;
+                        ret[node.boundary_index]
+                            .array[count++]
+                            .coefficient_index = connected.index;
                     }
                 }
             }
@@ -91,72 +105,101 @@ public:
     cl_int compute_boundary_type(const Locator& loc,
                                  const std::vector<Node>& ret) const;
 
-    struct Connected {
-        cl_uint index;
-        cl_int type;
-    };
+    class ConnectedFinder {
+    public:
+        struct Connected {
+            cl_uint index;
+            cl_int type;
+        };
 
-    template <typename B>
-    Connected look_for_connected(const B& b,
-                                 const Node& node,
-                                 const std::vector<Node>& ret,
-                                 RectangularProgram::BoundaryType bt) const {
-        assert(node.boundary_type != RectangularProgram::id_none);
+        using MemoizeKey = std::pair<std::vector<Node>::size_type,
+                                     RectangularProgram::BoundaryType>;
 
-        //  if this is a 1d boundary in the correct direction
-        if (node.boundary_type == bt) {
-            //  return the coefficient index
-            return Connected{coefficient_index_for_node(b, node), bt};
+        template <typename B>
+        Connected look_for_connected_memoized(
+            const B& b,
+            std::vector<Node>::size_type node_index,
+            const std::vector<Node>& ret,
+            RectangularProgram::BoundaryType bt) {
+            auto key = MemoizeKey{node_index, bt};
+            auto found = memoize_data.find(key);
+            if (found != memoize_data.end()) {
+                return found->second;
+            }
+
+            auto connected = look_for_connected(b, node_index, ret, bt);
+            memoize_data[key] = connected;
+            return connected;
         }
 
-        //  if this is a reentrant node
-        if (node.boundary_type == RectangularProgram::id_reentrant) {
-            return Connected{coefficient_index_for_node(b, node),
-                             RectangularProgram::id_reentrant};
-        }
+        template <typename B>
+        Connected look_for_connected(const B& b,
+                                     std::vector<Node>::size_type node_index,
+                                     const std::vector<Node>& ret,
+                                     RectangularProgram::BoundaryType bt) {
+            const auto& node = ret[node_index];
+            assert(node.boundary_type != RectangularProgram::id_none);
 
-        //  if this is a 1d boundary in the wrong direction
-        if (popcount(node.boundary_type) == 1) {
+            //  if this is a 1d boundary in the correct direction
+            if (node.boundary_type == bt) {
+                //  return the coefficient index
+                return Connected{coefficient_index_for_node(b, node), bt};
+            }
+
+            //  if this is a reentrant node
+            if (node.boundary_type == RectangularProgram::id_reentrant) {
+                return Connected{coefficient_index_for_node(b, node),
+                                 RectangularProgram::id_reentrant};
+            }
+
+            //  if this is a 1d boundary in the wrong direction
+            if (popcount(node.boundary_type) == 1) {
+                return Connected{0, RectangularProgram::id_none};
+            }
+
+            //  it's a higher order boundary
+
+            //  check each adjacent node
+            std::vector<Connected> nearby;
+            for (auto i = 0; i != PORTS; ++i) {
+                //  if there is (supposedly) a lower-order boundary node in this
+                //  direction
+                auto bits = RectangularProgram::port_index_to_boundary_type(i);
+                if (node.boundary_type & bits) {
+                    auto adjacent_index = node.ports[i];
+                    //  if the node is in the mesh
+                    if (adjacent_index != RectangularProgram::NO_NEIGHBOR) {
+                        nearby.push_back(look_for_connected_memoized(
+                            b, adjacent_index, ret, bt));
+                    }
+                }
+            }
+
+            assert(nearby.size() == popcount(node.boundary_type));
+
+            auto ideal_it = std::find_if(nearby.begin(),
+                                         nearby.end(),
+                                         [bt](auto i) { return i.type == bt; });
+            if (ideal_it != nearby.end()) {
+                return *ideal_it;
+            }
+
+            auto ok_it = std::find_if(
+                nearby.begin(),
+                nearby.end(),
+                [](auto i) {
+                    return i.type == RectangularProgram::id_reentrant;
+                });
+            if (ok_it != nearby.end()) {
+                return *ok_it;
+            }
+
             return Connected{0, RectangularProgram::id_none};
         }
 
-        //  it's a higher order boundary
-
-        //  check each adjacent node
-        std::vector<Connected> nearby;
-        for (auto i = 0; i != PORTS; ++i) {
-            //  if there is (supposedly) a lower-order boundary node in this
-            //  direction
-            auto bits = RectangularProgram::port_index_to_boundary_type(i);
-            if (node.boundary_type & bits) {
-                auto adjacent_index = node.ports[i];
-                //  if the node is in the mesh
-                if (adjacent_index != RectangularProgram::NO_NEIGHBOR) {
-                    const auto& adjacent = ret[adjacent_index];
-                    nearby.push_back(look_for_connected(b, adjacent, ret, bt));
-                }
-            }
-        }
-
-        assert(nearby.size() == popcount(node.boundary_type));
-
-        auto ideal_it = std::find_if(nearby.begin(),
-                                     nearby.end(),
-                                     [bt](auto i) { return i.type == bt; });
-        if (ideal_it != nearby.end()) {
-            return *ideal_it;
-        }
-
-        auto ok_it = std::find_if(
-            nearby.begin(),
-            nearby.end(),
-            [](auto i) { return i.type == RectangularProgram::id_reentrant; });
-        if (ok_it != nearby.end()) {
-            return *ok_it;
-        }
-
-        return Connected{0, RectangularProgram::id_none};
-    }
+    private:
+        std::unordered_map<MemoizeKey, Connected> memoize_data;
+    };
 
 private:
     template <typename B>
