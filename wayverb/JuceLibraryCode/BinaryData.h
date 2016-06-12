86b72cd2da1a89ad56700e48b79d1b55fdde7a25
reuk <reuben.thomas@me.com>
2016-06-12 16:16:06 +0100

    freshenned wayverb.xcproj

--- wayverb/JuceLibraryCode/BinaryData.h
+++ wayverb/JuceLibraryCode/BinaryData.h
@@ -9,9 +9,6 @@
 
 namespace BinaryData
 {
-    extern const char*   HackRegular_otf;
-    const int            HackRegular_otfSize = 161056;
-
     extern const char*   wayverb_png;
     const int            wayverb_pngSize = 205375;
 
@@ -19,7 +16,7 @@ namespace BinaryData
     extern const char* namedResourceList[];
 
     // Number of elements in the namedResourceList array.
-    const int namedResourceListSize = 2;
+    const int namedResourceListSize = 1;
 
     // If you provide the name of one of the binary resource variables above, this function will
     // return the corresponding data and its size (or a null pointer if the name isn't found).
