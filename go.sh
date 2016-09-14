1a1e51416b2b4a148e5e70c3b03c883c088d8a14
reuk <reuben.thomas@me.com>
2016-09-14 13:42:56 +0100

    boundary test update

--- go.sh
+++ go.sh
@@ -26,10 +26,10 @@ callraytrace () {
 export GLOG_logtostderr=1
 #export GTEST_FILTER="*run_waveguide*"
 
-#cmake .. && make && ctest -V
+cmake .. && make #&& ctest -V
 #cmake .. && make && cd utils/image_source_comparison && ./image_source_comparison
 
-cmake .. && make && cd utils/southern2013_2_cuboid && ./southern2013_2_cuboid
+#cmake .. && make && cd utils/southern2013_2_cuboid && ./southern2013_2_cuboid
 
 #cmake .. && make && make doc && ctest -V
 #cmake .. && make && ctest -V && callraytrace vault vault vault
